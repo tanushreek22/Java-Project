1dcee8c52747020ca138ba4e40c352063c54daed
tanushreek22 <tanushreekhobragade2020@gmail.com>
2024-08-04 23:52:34 +0530

    Add files via upload

---
+++ Accounts.java
@@ -0,0 +1,24 @@
+package BankingManagementSystem;
+
+public class Accounts {
+
+    public long openAccount(String email) {
+
+        return 0;// temporary
+    }
+
+    public long AccountNum(String email) {
+
+        return 0;// temporary
+    }
+
+    public boolean generateAcountNum() {
+
+        return false;// temporary
+    }
+
+    public boolean accountExist() {
+
+        return false;
+    }
+}
