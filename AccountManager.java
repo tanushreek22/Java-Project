6e016a8aeff4101e9528c838ff5f49ae2ff837d0
tanushreek22 <tanushreekhobragade2020@gmail.com>
2024-08-04 23:48:36 +0530

    Create AccountManager.java

---
+++ AccountManager.java
@@ -0,0 +1,18 @@
+package BankingManagementSystem;
+
+public class AccountManager {
+
+	public void creditAccount() {
+		
+	}
+	
+	public void debitMoney() {
+		
+	}
+	public void transferMoney() {
+		
+	}
+	public void checkBalance() {
+		
+	}
+}
